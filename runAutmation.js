6b1b98a491c0a84303821bc97b42170fee1acc6f
sculouis <scu.louis@gmail.com>
2020-07-21 17:21:56 +0800

    Update runAutmation.js

--- runAutmation.js
+++ runAutmation.js
@@ -2,10 +2,12 @@ var example = require("./webAutomation");
 //定義測試資料
 //type:1.一般資料,2.按鈕,3.radioButton,4.checkBox
 var getTestData = require("./readData");
-var filename = `${__dirname}\\Sample.xlsx`;
+var config = require("./config.json")
+var filename = `${__dirname}\\${config.fileName}`;
 
-var sheetName = "法人";
+var sheetName = config.sheetName;
 getTestData(filename,sheetName).then((data)=>{
     console.log(data);
     example(data);    
 });
+
